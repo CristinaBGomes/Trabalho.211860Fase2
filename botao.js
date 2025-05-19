20e23a35a23271ed77e91d5fe6ba846cb44719cd
CRISTINABGOMES <cristina.braga.gomrs@gmail.com>
2025-05-18 22:09:21 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,7 @@
+document.getElementById("p").style.backgroundColor="red";
+document.getElementById("p").style.color="white";
+document.getElementById("p").style.fontSize="30px";
+document.getElementById("p").style.fontFamily="Viaoda Libre";
+document.getElementById("p").style.fontWeight="bold";
+
+document.getElementById("rodape").style.backgroundColor="orange";
